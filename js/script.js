f61c536a8b0589bff599d3abb9b097b2d0629e74
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-18 16:43:13 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-jakub-malhotra/sw.js", {
+    scope: "/ICS2O-Unit-6-01-jakub-malhotra/",
+  })
+}
+
+/**
+ * This function calculates fahrenheit to celsius.
+ */
+function enterClicked() {
+  //input
+  const fahrenheittemp = parseFloat(
+    document.getElementById("fahrenheittemp").value
+  )
+
+  //process
+  const celsiustemp = ((fahrenheittemp - 32) * 5) / 9
+
+  //output
+  document.getElementById("celsiustemp").innerHTML =
+    "The temperture in celsius is: " + celsiustemp.toFixed(2) + " °C."
+}
